a710793678d5f48cd493500567de8a04cff4f2cc
Mark Phillipson <MPhillipson0@Gmail.com>
2022-03-03 17:26:12 +0000

    Add project files.

---
+++ ARM_MVC/Authentication/CustomClaimsTransformation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARM_MVC.Authentication
+{
+    public class CustomClaimsTransformation : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            // Proposed to get role names For the current user User.Identity.Name from database here then iterate over the following code To add each 1 as applicable
+
+            if (principal!= null  && principal?.Identity!= null)
+            {
+                ClaimsIdentity? claimsIdentity = (ClaimsIdentity)principal.Identity;
+                var claim = new Claim(claimsIdentity.RoleClaimType,"BasicUser");
+                claimsIdentity.AddClaim(claim);
+                claim = new Claim(claimsIdentity.RoleClaimType, "Admin");
+                claimsIdentity.AddClaim(claim);
+                return Task.FromResult(principal);
+            }
+            throw new NotImplementedException("principle or identity was null!");
+        }
+    }
+}
+
+
---
+++ ARM_MVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ARM_MVC.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ARM_MVC.Controllers
+{
+    //[Authorize(Roles ="Administration")]
+    
+    //[Authorize(Users= "DESKTOP-UROO8T1\\MPhil")]
+    //https://stackoverflow.com/questions/60105635/create-role-based-authorization-with-windows-authentication
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IClaimsTransformation _claimsTransformation;
+
+        public HomeController(ILogger<HomeController> logger)//,IClaimsTransformation claimsTransformation)
+        {
+            _logger = logger;
+            //_claimsTransformation = claimsTransformation;
+            
+        }
+        [Authorize(Roles ="BasicUser")]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Authorize(Roles ="SuperUser")]
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
